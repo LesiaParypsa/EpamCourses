afd715168c6560db0f57b90def32aaab1afe987c
Lesia Parypsa <mile3824@gmail.com>
2019-02-03 12:48:20 +0200

    update test

--- src/main/java/homework/lesson9/SeleniumTest.java
+++ src/main/java/homework/lesson9/SeleniumTest.java
@@ -7,28 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
 public class SeleniumTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "seleniumDrivers\\chromedriver.exe");
 
-        WebDriver testDriver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=en_GB");
+
+        WebDriver testDriver = new ChromeDriver(chromeOptions);
 
         testDriver.get("https://www.google.com");
         WebElement googleSearchField = testDriver.findElement(By.xpath("//input[@name=\"q\"]"));
         googleSearchField.sendKeys("Facebook");
 
-//        WebElement googleSearchButton = testDriver.findElement(By.xpath("//div[@class=\"VlcLAe\"]//input[@name=\"btnK\"]"));
-//        googleSearchButton.click();
-
-        googleSearchField.sendKeys(Keys.ENTER);
+        //        googleSearchField.sendKeys(Keys.ENTER);
+        // or
+        WebElement googleSearchButton = testDriver.findElement(By.xpath("//div[@class=\"VlcLAe\"]//input[@name=\"btnK\"]"));
+        googleSearchButton.submit();
 
         WebElement googleSearchResult = testDriver.findElement(By.xpath("//h3[@class=\"LC20lb\"]"));
         googleSearchResult.click();
 
         WebElement fbEmailInput = testDriver.findElement(By.xpath("//input[@id=\"email\"]"));
-        fbEmailInput.sendKeys("milemil21312com");
+        fbEmailInput.sendKeys("mile3824@gmail.com");
 
         WebElement fbPasswordInput = testDriver.findElement(By.xpath("//input[@id=\"pass\"]"));
         fbPasswordInput.sendKeys("mile mile");
@@ -37,7 +38,8 @@ public class SeleniumTest {
         fbSignInBtn.click();
 
         WebElement fbWarning = testDriver.findElement(By.xpath("//div[@class=\"_4rbf _53ij\"]"));
-        if (fbWarning.getText().equals("Указана електронна адреса (або номер телефону) не пов'язана з жодним обліковим записом. ")) {
+        if (fbWarning.getText().equals("The email address that you've entered doesn't match any account. Sign up for an account.")) {
+//        if (fbWarning.getText().equals("The email address or phone number that you've entered doesn't match any account. Sign up for an account.")) {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
